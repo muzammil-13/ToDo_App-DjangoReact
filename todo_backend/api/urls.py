c5a659b94335b34f37c040a8246bfd4da0dbb41d
Muzammil Ibrahim <muzammilibrahim13@gmail.com>
2024-09-18 11:34:45 +0530

    Setup Authentication – Log In Tokens

--- todo_backend/api/urls.py
+++ todo_backend/api/urls.py
@@ -6,4 +6,6 @@ urlpatterns=[
     path('todoApp/<int:pk>',views.TodoRetrieveUpdateDestroy.as_view()),
     path('todoApp/<int:pk>/complete',views.TodoToggleComplete.as_view()),
     path('signup/',views.signup),
+    path('signup/',views.signup),
+    path('login/',views.login),
 ]
--- todo_backend/api/views.py
+++ todo_backend/api/views.py
@@ -7,6 +7,7 @@ from rest_framework.parsers import JSONParser
 from rest_framework.authtoken.models import Token
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import authenticate
 
 
 
@@ -66,3 +67,22 @@ def signup(request):
                 {'error':'username taken. Choose another username'},
                 status=400
             )
+        
+@csrf_exempt
+def login(request):
+    if request.method=='POST':
+        data=JSONParser().parse(request)
+        user=authenticate(
+            request,
+            username=data['username'],
+            password=data['password'])
+        if user is None:
+            return JsonResponse(
+                        {'error':'unable to login. check username and password'},
+                        status=400)
+        else: # return user token
+            try:
+                token=Token.objects.get(user=user)
+            except: # if token not in db, create a new one
+                token=Token.objects.create(user=user)
+            return JsonResponse({'token':str(token)},status=201)
