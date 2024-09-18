a634495d3cbf708d11f0ca718789c41ceab64147
Muzammil Ibrahim <muzammilibrahim13@gmail.com>
2024-09-18 15:55:28 +0530

    Create Navigation Header Bar

--- todo_frontend/src/App.js
+++ todo_frontend/src/App.js
@@ -2,13 +2,59 @@ import React from 'react';
 import { Switch,Route,Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// import components
+import AddTodo from './components/add-todo';
+import TodoList from './components/todos-list';
+import Login from './components/login';
+import Signup from './components/signup';
+
+// Navbar with react-bootstrap
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Navbar';
 
 function App(){
+  // const user=null;
+  const [user, setUser]=React.useState(null);
+  const [token, setToken]=React.useState(null);
+  const [error, setError]=React.useState("");
+
+  async function login(user=null){ // default user to null
+    setUser(user);
+  }
+
+  async function logout(){
+    setUser(null);
+  }
+
+  async function signup(user=null){ // default user to null
+    setUser(user);
+  }
+
   return(
     <div className="App">
+      <Navbar bg="primary" variant="dark">
+        <div className="container-fluid">
+          <Navbar.Brand>Todo App</Navbar.Brand>
+          <Nav className="me-auto">
+            <Container>
+              <Link class="nav-link" to={"/todos"}>Todos</Link>
+              {user ?(
+                <Link class="nav-link">Logout ({user})</Link>
+              ):(
+                <>
+                <Link class="nav-link" to={"/login"}>Login</Link>
+                <Link class="nav-link" to={"/signup"}>Sign Up</Link>
+                </>
+              )}
+            </Container>
+          </Nav>
+        </div>
+      </Navbar>
       <h3 style={{textAlign:'center'}}>Hello React App</h3>
     </div>
-  )
+    
+  );
 }
 
 export default App;
---
+++ todo_frontend/src/components/todos-list.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TodosList(){
+    return (
+    <div className="App">
+        Todo List
+    </div>
+    );
+}
+
+export default TodosList;
