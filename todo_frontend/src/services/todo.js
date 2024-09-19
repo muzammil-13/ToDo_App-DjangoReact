145c912733f72d5c3f59786f01e766ae49b05587
Muzammil Ibrahim <muzammilibrahim13@gmail.com>
2024-09-19 16:49:05 +0530

    updated Readme

---
+++ todo_frontend/src/services/todo.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+// We use a library called axios for sending get, post, put and delete requests.
+
+class TodoDataService{
+
+    getAll(token){
+        axios.defaults.headers.command['Authorization']="Token"+token;
+        return axios.get("http://localhost:8000/api/todoApp/")
+    }
+
+    createTodo(data,token){
+        axios.defaults.headers.common["Authorization"]="Token"+token;
+        return axios.post("http://127.0.0.1:8000/api/todoApp/",data);
+    }
+
+    updateTodo(id,data,token){
+        axios.defaults.headers.common["Authorization"]="Token"+token;
+        return axios.put("http://127.0.0.1:8000/api/todoApp/${id}",data);
+    }
+
+    deleteTodo(id,token){
+        axios.defaults.headers.common["Authorization"]="Token"+token;
+        return axios.delete("http://127.0.0.1:8000/api/todoApp/${id}/complete");
+    }
+
+    completeTodo(id,token){
+        axios.default.headers.common["Authorization"]="Token"+token;
+        return axios.put("http://127.0.0.1:8000/api/todoApp/${id}/complete");
+    }
+
+    login(data){
+        return axios.post("http://127.0.0.1:8000/api/login/",data);
+    }
+
+    singup(data){
+        return axios.post("http://127.0.0.1:8000/api/signup/",data);
+    }
+}
+
+export default new TodoDataService();
